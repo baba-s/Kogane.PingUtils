38c5b998cc7f9432ec6e50f49fabe1db67481248
baba-s <shotababa1206@gmail.com>
2023-01-01 13:32:55 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Kogane.PingUtils
+﻿# Kogane Ping Utils
+
+UnityEngine.Ping を非同期処理で呼び出せるようにするクラス
+
+## 使用例
+
+```cs
+using Cysharp.Threading.Tasks;
+using Kogane;
+
+public static class InternetChecker
+{
+    private static readonly string[] ADDRESSES =
+    {
+        "8.8.8.8", // Google Public DNS
+        "8.8.4.4", // Google Public DNS
+        "4.2.2.2", // Level 3 Communications
+        "4.2.2.3", // Level 3 Communications
+        "4.2.2.4", // Level 3 Communications
+    };
+
+    public static async UniTask<bool> IsOnlineAsync( float timeoutSeconds )
+    {
+        foreach ( var address in ADDRESSES )
+        {
+            var result = await PingUtils.SendAsync( address, timeoutSeconds );
+
+            if ( result.IsSuccess ) return true;
+        }
+
+        return false;
+    }
+}
+```
+
+```cs
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    private async UniTaskVoid Start()
+    {
+        Debug.Log( await InternetChecker.IsOnlineAsync( 5 ) );
+    }
+}
+```
---
+++ Runtime/PingUtils.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Kogane
+{
+    /// <summary>
+    /// UnityEngine.Ping を非同期処理で呼び出せるようにするクラス
+    /// </summary>
+    public static class PingUtils
+    {
+        //================================================================================
+        // 構造体
+        //================================================================================
+        [Serializable]
+        public struct Result
+        {
+            [SerializeField] private bool m_isSuccess;
+            [SerializeField] private int  m_timeMilliseconds;
+
+            /// <summary>
+            /// ping が届いて返ってきたら true を返します
+            /// </summary>
+            public bool IsSuccess => m_isSuccess;
+
+            /// <summary>
+            /// ping が返ってくるまでにかかった時間（ミリ秒）を返します
+            /// </summary>
+            public int TimeMilliseconds => m_timeMilliseconds;
+
+            internal Result
+            (
+                bool isSuccess,
+                int  timeMilliseconds
+            )
+            {
+                m_isSuccess        = isSuccess;
+                m_timeMilliseconds = timeMilliseconds;
+            }
+
+            public override string ToString()
+            {
+                return JsonUtility.ToJson( this, true );
+            }
+        }
+
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// IP アドレスに対して ping を実行します
+        /// </summary>
+        public static async UniTask<Result> SendAsync( string address, float timeoutSeconds )
+        {
+            // 機内モードや Wi-Fi がオフならインターネットに接続していません
+            if ( Application.internetReachability == NetworkReachability.NotReachable ) return new( false, -1 );
+
+            // ping を飛ばすことでインターネットに接続しているかどうか確認します
+            var ping      = new Ping( address );
+            var startTime = Time.time;
+
+            while ( true )
+            {
+                // ping が届いて返ってきたらインターネットに接続しているとみなします
+                if ( ping.isDone ) return new( true, ping.time );
+
+                // タイムアウトするまで待機します
+                if ( Time.time - startTime < timeoutSeconds )
+                {
+                    await UniTask.Yield();
+                }
+                // タイムアウトしたらインターネットに接続していないとみなします
+                else
+                {
+                    return new( false, -1 );
+                }
+            }
+        }
+    }
+}
